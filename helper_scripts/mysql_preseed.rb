8a4bd6a175b47421ca7e1406b56ebdbd5e1fec0b
Joseph Reynolds <reynoldsjoseph.f@gmail.com>
2013-05-11 17:40:39 -0400

    Add reminder for next step

--- helper_scripts/mysql_preseed.rb
+++ helper_scripts/mysql_preseed.rb
@@ -1,3 +1,4 @@
+#Todo seperate out into two files, one with and awk script to parse the details from the second file
 sudo apt-get -y install debconf-utils
 echo "mysql-server-5.5 mysql-server/root_password password $MYSQL_ROOT_PWD" > mysql.preseed
 echo "mysql-server-5.5 mysql-server/root_password_again password $MYSQL_ROOT_PWD" >> mysql.preseed
